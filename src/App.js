9e29077f9452511cfa63b53a46a65467f00cde61
Ledjan Miçani <ledjanmicani@gmail.com>
2024-09-23 16:58:55 +0200

    Social links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Home from "./components/Home";
 import MyNavBar from "./components/MyNavBar";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
     <div>
       <MyNavBar />
       <Home />
+
+      <SocialLinks />
     </div>
   );
 }
