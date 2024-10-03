a6f71ef1ccba5f709f4ef1b92d69c8f76b92ea93
lmicani <ledjanmicani@gmail.com>
2024-10-03 12:21:44 +0200

    update

---
+++ src/components/Experience.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import html from '../assets/html.png'
+import css from '../assets/html.png'
+import javascript from '../assets/javascript.png'
+import reactimage from '../assets/react.png'
+import node from '../assets/node.png'
+import github from '../assets/github.png'
+import tailwind from '../assets/tailwind.png'
+
+const Experience = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <p>Experience</p>
+                <p>These are the technologies I've worked with</p>
+            </div>
+
+            <div>
+                <div>
+                    <img src={html} alt="" />
+                    <p>HTML</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
